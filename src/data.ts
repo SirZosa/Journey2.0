9073351bcc638068da5adefd67adc6e3129a1111
Omar Sosa <omar.sosa.quimica@gmail.com>
2024-10-15 18:18:14 -0600

    Change in data img string format

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ export const posts = [
         date:"SEP 30, 2024",
         title:"Restaurant",
         message:"A responsive single page application.",
-        img:"src/img/restaurant.webp",
+        img:"src\\img\\restaurant.webp",
         alt:"restaurant image",
         link:"https://omars-restaurant.netlify.app/",
         codeLink:"https://github.com/SirZosa/restaurant"
@@ -12,7 +12,7 @@ export const posts = [
         date:"SEP 18, 2024",
         title:"Cool typing game",
         message:"A game to test your typing speed!",
-        img:"src/img/typegame.webp",
+        img:"src\\img\\typegame.webp",
         alt:"game image",
         link:"https://omars-typing-game.netlify.app/",
         codeLink:"https://github.com/SirZosa/TypeGame"
@@ -21,7 +21,7 @@ export const posts = [
         date:"JUNE 22, 2024",
         title:"Vet Landing Page",
         message:"A simple landing page for a veterinary.",
-        img:"src/img/vet.webp",
+        img:"src\\img\\vet.webp",
         alt:"Vet image",
         link:"https://omars-vet.netlify.app/",
         codeLink:"https://github.com/SirZosa/vet"
@@ -30,7 +30,7 @@ export const posts = [
         date:"JUNE 6, 2024",
         title:"E-commerce v2!",
         message:"Upgraded my E-commerce website to allow users create profiles using mySQL for the database and Node JS for the API.",
-        img:"src/img/omars-ecommerce.webp",
+        img:"src\\img\\omars-ecommerce.webp",
         alt:"Ecommerce image",
         link:"https://omars-commerce.netlify.app/",
         codeLink:"https://github.com/SirZosa/EcommerceAPI"
@@ -39,7 +39,7 @@ export const posts = [
         date:"APRIL 6, 2024",
         title:"Quizical V2",
         message:"My first app using typescript! A redesign of my app quizzical.",
-        img:"src/img/quizicalV2.webp",
+        img:"src\\img\\quizicalV2.webp",
         alt:"Quizicalv2 image",
         link:"https://omars-quizicalv2.netlify.app/",
         codeLink:"https://github.com/SirZosa/quizicalV2"
@@ -48,7 +48,7 @@ export const posts = [
         date:"MARCH 21, 2024",
         title:"E-commerce",
         message:"A simple Ecommerce website built using react router (SPA).",
-        img:"src/img/omars-ecommerce.webp",
+        img:"src\\img\\omars-ecommerce.webp",
         alt:"ecommerce image",
         link:"https://omars-ecommerce.netlify.app/",
         codeLink:"https://github.com/SirZosa/E-commerce"
@@ -57,7 +57,7 @@ export const posts = [
         date:"JANUARY 11, 2024",
         title:"Tensies",
         message:"Practicing my coding skills by building a tensies game.",
-        img:"src/img/tensies.webp",
+        img:"src\\img\\tensies.webp",
         alt:"tensies image",
         link:"https://omars-tensies-game.netlify.app/",
         codeLink:"https://github.com/SirZosa/tensies"
@@ -66,7 +66,7 @@ export const posts = [
         date:"NOVEMBER 1, 2023",
         title:"Tic Tac Toe",
         message:"Practicing my coding skills by building a tic tac toe game.",
-        img:"src/img/tictactoe.webp",
+        img:"src\\img\\tictactoe.webp",
         alt:"Tic tac toe image",
         link:"https://omars-tic-tac-toe.netlify.app/",
         codeLink:"https://github.com/SirZosa/tictactoe"
@@ -75,7 +75,7 @@ export const posts = [
         date:"SEPTEMBER 12, 2023",
         title:"Small library",
         message:"I learned about compound components and it's benefits, especially in React to avoid 'prop drilling', so i decided to create a small library.",
-        img:"src/img/library.webp",
+        img:"src\\img\\library.webp",
         alt:"library image",
         link:"https://omars-small-library.netlify.app/",
         codeLink:"https://github.com/SirZosa/small-library"
@@ -84,7 +84,7 @@ export const posts = [
         date:"AUGUST 18, 2023",
         title:"VanLife",
         message:"A single page React/Vite app that simulates a rental website, where the user can filter between the opcions the store offer. The website was built in Vite using React router 6.",
-        img:"src/img/vans.png",
+        img:"src\\img\\vans.png",
         alt:"Vans app image",
         link:"https://omars-vanlife.netlify.app/",
         codeLink:"https://github.com/SirZosa/VanLife"
@@ -93,7 +93,7 @@ export const posts = [
         date:"JULY 18, 2023",
         title:"Quizzical",
         message:"A trivia web site built in next.js, a react framework. The web site basically uses an api that return 5 questions and the page let the user to interact with those questions trying to select the rigth answer.",
-        img:"src/img/quizzical.webp",
+        img:"src\\img\\quizzical.webp",
         alt:"quizzical app image",
         link:"https://omars-quizzical-site.netlify.app/",
         codeLink:"https://github.com/SirZosa/quizzical"
@@ -102,7 +102,7 @@ export const posts = [
         date:"JUNE 28, 2023",
         title:"First react app",
         message:"My first page made with React! At the beggining it was confusion how to setup all the stuff tu begin with but at the end is almost the same thing as vanilla js at the moment. I made a static webpage about me and my contact information.",
-        img:"src/img/react.webp",
+        img:"src\\img\\react.webp",
         alt:"First react app image",
         link:"https://omars-first-react-app.netlify.app/",
         codeLink:"https://github.com/SirZosa/first-react-app"
@@ -111,7 +111,7 @@ export const posts = [
         date:"JUNE 17, 2023",
         title:"Movie Watchlist",
         message:"My second project working with an API, this time I needed to send to the API the search of a movie and the API sent me information of 10 movies that matched the search. First I tried working with the localStore of the device and worked, but for a reason when uploaded to the internet it doesn't work, so I needed to find another way of making it work. That's when I learned about the in-built database in the brower indexedDB.",
-        img:"src/img/watchlist.webp",
+        img:"src\\img\\watchlist.webp",
         alt:"Movie Watchlist image",
         link:"https://omars-watchlist.netlify.app/",
         codeLink:"https://github.com/SirZosa/watchlist"
@@ -120,7 +120,7 @@ export const posts = [
         date:"JUNE 8, 2023",
         title:"Color generator",
         message:"This was my first project working with an API, sending color values to the API and receiving information from the API, it was a little tricky to access to that information but at the end I managed to get the app working.",
-        img:"src/img/color.webp",
+        img:"src\\img\\color.webp",
         alt:"Color generator image",
         link:"https://omars-color-generator.netlify.app/",
         codeLink:"https://github.com/SirZosa/Color-Generator"
@@ -129,7 +129,7 @@ export const posts = [
         date:"JUNE 1, 2023",
         title:"Tindog",
         message:"I have created an app like tinder but for dogs, it just let you do the basic stuff, just swipe like or nope, it uses classes and a constructor function to render the dogs in the app.",
-        img:"src/img/tindog.webp",
+        img:"src\\img\\tindog.webp",
         alt:"Tindog image",
         link:"https://tindog-sosa.netlify.app/",
         codeLink:"https://github.com/SirZosa/Tindog"
@@ -138,7 +138,7 @@ export const posts = [
         date:"May 9, 2023",
         title:"Champion's Site",
         message:"In this project I worked mostly with JavaScrip. It was a challenge since I was putting into practice the new things I was learning. But at the end I make it work as I intended.",
-        img:"src/img/champ.webp",
+        img:"src\\img\\champ.webp",
         alt:"Champ's Site image",
         link:"https://omars-champion-site.netlify.app/",
         codeLink:"https://github.com/SirZosa/Champions-site"
@@ -147,7 +147,7 @@ export const posts = [
         date:"APRIL 18, 2023",
         title:"My piano's Site",
         message:"I wanted to practice what I've learned about HTML and CSS, so the topic of the site I wanted to create is my piano. In this site I show all the songs I've learned to play on the piano.",
-        img:"src/img/piano.webp",
+        img:"src\\img\\piano.webp",
         alt:"My piano's Site image",
         link:"https://omars-piano.netlify.app/",
         codeLink:"https://github.com/SirZosa/pianosite"
@@ -156,7 +156,7 @@ export const posts = [
         date:"MAY 7, 2023",
         title:"Order Online Pls",
         message:"In this project I was asked to make an mobile app for oderdering food online, for some reason it does not work in netlify but anyways I added the link to the site.",
-        img:"src/img/food.webp",
+        img:"src\\img\\food.webp",
         alt:"order online app image",
         link:"https://order-onlinepls.netlify.app/",
         codeLink:"https://github.com/SirZosa/Restaurant-app"
@@ -165,7 +165,7 @@ export const posts = [
         date:"APRIL 4, 2023",
         title:"Practice website",
         message:"This was my first responsive website I made watching a youtube tutorial that teached me the basics of HTML and CSS.",
-        img:"src/img/responsive.webp",
+        img:"src\\img\\responsive.webp",
         alt:"Responsive website image",
         link:"https://sirzosa-practice-website.netlify.app/",
         codeLink:"https://github.com/SirZosa/sirzosa.github.io"
@@ -174,7 +174,7 @@ export const posts = [
         date:"APRIL 6, 2023",
         title:"Chrome extension",
         message:"My first web browser extension. Save the current tab's URL into a list so you can go back to that page later on. The extension also allows to save custom inputs and revome the last item on the list.",
-        img:"src/img/extension.webp",
+        img:"src\\img\\extension.webp",
         alt:"Web browser extension image",
         link:"https://github.com/SirZosa/Chrome-extension-practice",
         codeLink: "https://github.com/SirZosa/Chrome-extension-practice"
